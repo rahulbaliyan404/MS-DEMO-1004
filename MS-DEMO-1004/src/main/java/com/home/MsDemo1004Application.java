0c60b8b8b22bb4ccbb1c42569972f4c1b0960b0b
rahulbaliyan404 <rahulbaliyan404@gmail.com>
2021-12-15 21:13:35 +0530

    This is first commit of MS-DEMO-1004 on 15-12-2021.

--- MS-DEMO-1004/src/main/java/com/home/MsDemo1004Application.java
+++ MS-DEMO-1004/src/main/java/com/home/MsDemo1004Application.java
@@ -7,7 +7,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @EnableDiscoveryClient
 @SpringBootApplication
 public class MsDemo1004Application {
-
+// This DEMO-1004 MS Service main method.
 	public static void main(String[] args) {
 		
 		SpringApplication.run(MsDemo1004Application.class, args);
